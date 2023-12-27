7c5cfbd42ffa5971d2eb2c7550c3d805d8559612
Chinnupinky10 <thummalapravallika4@gmail.>
2023-12-27 19:32:02 +0530

    first commit

---
+++ app2/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Country(models.Model):
+    Cname=models.CharField(max_length=100,primary_key=True)
+    Ccode=models.CharField(max_length=100)
